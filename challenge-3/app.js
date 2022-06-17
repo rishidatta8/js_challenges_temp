154421aefa4cec951bfb56cd10218ef87f8f5f41
Rishi Datta <62599930+rishidatta8@users.noreply.github.com>
2022-06-17 15:32:53 +0530

    Add files via upload
    Refactored challenge-3 index.js

--- challenge-3/app.js
+++ challenge-3/app.js
@@ -1,22 +1,15 @@
 let keys = document.getElementsByTagName('a');
 
-const playSound = () => {
+const playSound = (index, sound) => {
     sound.src = './audio/key-' + index + '.mp3';
-    index++;
     sound.play()
 };
 
 for (let i = 0; i < keys.length; i++) {
     const sound = new Audio()
-    keys[i].onclick = (e)=>{
-        e.preventDefault()
-        const playSound = () => {
-            sound.src = './audio/key-'+(i+1)+'.mp3';
-            sound.play()
-        };
-        playSound();
-        remove_hash_from_url()
+    keys[i].onclick = event =>{
+        event.preventDefault()
+        playSound(i+1, sound);
     }
 }
-
 
